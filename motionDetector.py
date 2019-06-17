1e00c31b2f2fd5318bd7fe6b7c60e821cc98a62d
Abhirav Kariya <abhiravredox@gmail.com>
2019-06-18 00:22:26 +0530

    Update motionDetector.py

--- motionDetector.py
+++ motionDetector.py
@@ -12,15 +12,18 @@ def main():
 	frameMaskOld = cv2.morphologyEx(frameMaskOld, cv2.MORPH_OPEN, se)
 	while(True):
 		ret, frame = cap.read()
-		frameMask = fgbg.apply(frame)
-		# frame = cv2.filter2D(frame,frameMask)
-		frameMask = cv2.morphologyEx(frameMask, cv2.MORPH_OPEN, se)
-		diff = cv2.subtract(frameMask, frameMaskOld)
-		if cv2.mean(diff)[0] > 1:
-			print("Motion Detected at " + str(datetime.datetime.now()))
-		cv2.imshow("Video", diff)
-		frameMaskOld = frameMask
-		if cv2.waitKey(1) & 0xFF == ord('q'):
+		if ret:
+			frameMask = fgbg.apply(frame)
+			# frame = cv2.filter2D(frame,frameMask)
+			frameMask = cv2.morphologyEx(frameMask, cv2.MORPH_OPEN, se)
+			diff = cv2.subtract(frameMask, frameMaskOld)
+			if cv2.mean(diff)[0] > 1:
+				print("Motion Detected at " + str(datetime.datetime.now()))
+			cv2.imshow("Video", diff)
+			frameMaskOld = frameMask
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		else:
 			break
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
